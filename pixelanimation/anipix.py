0d029b9cf81edeae6aea048cd92db9f9683abbb0
Hayden Ringer <hjrrockies@gmail.com>
2019-06-22 16:01:24 -0400

    usability of anipix.py

--- pixelanimation/anipix.py
+++ pixelanimation/anipix.py
@@ -1,13 +1,33 @@
+USAGE = """USAGE
+
+$ python anipix.py [imfile1] [imfile2] [outfile]
+
+[outfile] must be .mp4
+
+Example:
+
+    $ python anipix.py camera2.png lena.png mixing.mp4
+"""
+
+
 if __name__ == "__main__":
     from sys import argv
     import pixelanimation as pa
     from imageio import imread
     import numpy as np
 
-    img1 = np.array(imread(argv[1],as_gray=True))/256
-    img2 = np.array(imread(argv[2],as_gray=True))/256
+    if len(argv) < 4 or "--help" in argv:
+        print(USAGE)
 
-    if img1.shape[0]*img1.shape[1] != img2.shape[0]*img2.shape[1]:
-        raise ValueError("Images must have the name number of pixels")
-        
-    pa.animate_pixels(img1,img2,argv[3],verbose=True)
+    elif argv[3][-4:] != ".mp4":
+        print("Bad input: [outfile] must be .mp4\n")
+        print(USAGE)
+
+    else:
+        img1 = np.array(imread(argv[1],as_gray=True))/256
+        img2 = np.array(imread(argv[2],as_gray=True))/256
+
+        if img1.shape[0]*img1.shape[1] != img2.shape[0]*img2.shape[1]:
+            raise ValueError("Images must have the name number of pixels")
+
+        pa.animate_pixels(img1,img2,argv[3],verbose=True)
